94a7e6acbebf18826f9c702e37baec3a7051d466
Vitor <vitorguilherme@geb.inatel.br>
2023-03-14 16:10:09 -0300

    organizar todos os textos em um único arquivo

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -3,16 +3,16 @@ import { View, Image, StyleSheet } from 'react-native';
 import Texto from "../../../componentes/Texto";
 import logo from '../../../../assets/logo.png';
 
-export default function Detalhes() {
+export default function Detalhes({nome, logoFazenda, nomeFazenda, descricao, preco}) {
     return <>
-        <Texto style={estilos.nome}>Cesta de Verduras</Texto>
+        <Texto style={estilos.nome}>{nome}</Texto>
         <View style={estilos.fazenda}>
-            <Image source={logo} style={estilos.imagemFazenda} />
-            <Texto style={estilos.nomeFazenda}>Jenny Jack</Texto>
+            <Image source={logoFazenda} style={estilos.imagemFazenda} />
+            <Texto style={estilos.nomeFazenda}>{nomeFazenda}</Texto>
         </View>
 
-        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a sua cozinha</Texto>
-        <Texto style={estilos.prreco}>R$ 40,00</Texto>
+        <Texto style={estilos.descricao}>{descricao}</Texto>
+        <Texto style={estilos.preco}>{preco}</Texto>
     </>
 
 }
@@ -42,7 +42,7 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26
     },
-    prreco: {
+    preco: {
         color: "#2a9f85",
         fontWeight: "bold",
         fontSize: 26,
